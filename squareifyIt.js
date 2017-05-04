77af51c3f002c76397a7d73d544cb6fa2d2a2ef7
Jarleborn <hj222hi@student.lnu.se>
2017-05-04 14:20:13 +0200

    patch object supprot

--- squareifyIt.js
+++ squareifyIt.js
@@ -1,12 +1,15 @@
 function squareifyIt(text) {
   let line = ''
+  if (typeof text === 'object') {
+    text = JSON.stringify(text, null, 2)
+  }
+
   for (var i = 0; i < text.length + 4; i++) {
     line+= '*'
   }
 
-  if (typeof text === 'object') {
-    text = JSON.stringify(text, null, 2)
-  }
+
+
   console.log(line);
   console.log('* '+text +' *');
   console.log(line);
