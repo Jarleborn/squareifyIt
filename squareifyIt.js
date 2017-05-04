dfe7fd0006aee0a43bd1b7dc197bf024a17fc33d
Jarleborn <hj222hi@student.lnu.se>
2017-05-04 14:12:56 +0200

    implemented support for objects

--- squareifyIt.js
+++ squareifyIt.js
@@ -3,6 +3,10 @@ function squareifyIt(text) {
   for (var i = 0; i < text.length + 4; i++) {
     line+= '*'
   }
+
+  if (typeof text === 'object') {
+    text = JSON.stringify(text, null, 2)
+  }
   console.log(line);
   console.log('* '+text +' *');
   console.log(line);
